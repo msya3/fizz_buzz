16198be2a8c7c0750ef49534f3776347c31484aa
Masaya Suzuki <for.git.msya3@gmail.com>
2014-04-04 02:27:08 +0900

    add comment

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,4 +1,32 @@
 #!/c/ruby193/bin/ruby
 
-puts("hoge")
+# About Fizz Buzz
+# http://ja.wikipedia.org/wiki/Fizz_Buzz
+
+# get Max number
+endNum = ARGV[0].to_i
+
+for i in 1..endNum
+
+  fizz = i%3
+  buzz = i%5
+
+  if fizz == 0 && buzz == 0 then
+
+    puts("FizzBuzz")
+
+  elsif fizz == 0 then
+
+    puts("Fizz")
+
+  elsif buzz == 0 then
+
+    puts("Buzz")
+
+  else
+
+    puts i
+
+  end
+end
 
